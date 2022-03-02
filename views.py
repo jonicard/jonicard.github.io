8925031c16d0299007719f9891e0b21244e3b497
jonny <jonicard@live.com>
2022-03-02 18:26:36 -0500

    views

--- views.py
+++ views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("<a href='#'>Hello, world. You're at the polls index.</a>")
+def index(request)
+    return HttpResponse("<link rel='stylesheet' href='style.css'><a href='#'>Hello, world. You're at the polls index.</a>")
 # Create your views here.
