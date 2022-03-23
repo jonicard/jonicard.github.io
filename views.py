fff6e0a0a3fcb4f7fd6429da7bd80a15c8f6f3aa
jonicard <jonicard@live.com>
2022-03-22 21:13:11 -0500

    deleted

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request)
-    return HttpResponse("<link rel='stylesheet' href='style.css'><a href='#'>Hello, world. You're at the polls index.</a>")
-# Create your views here.
